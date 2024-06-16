282570ef93626703acb1f507071e1ee97fbae6a2
Kehindeee <k.h.amusa@wlv.ac.uk>
2024-06-16 12:50:22 +0100

    feat: enhanced styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,27 @@
 import React from "react";
+import { Link } from "gatsby";
 import { useAuth } from "../auth-context";
 import Welcome from "../components/Welcome";
+import * as styles from "../components/index.module.css";
 
 const IndexPage = () => {
   const { currentUser } = useAuth();
 
   return (
-    <div>
+    <div className={styles.container}>
       {currentUser ? (
         <Welcome user={currentUser} />
       ) : (
-        <p>Loading...</p>
+        <div className={styles.introContainer}>
+          <h1 className={styles.heading}>Welcome to the Note Progressive Web App</h1>
+          <p className={styles.subheading}>
+            This is a progressive web app for taking notes efficiently and securely.
+          </p>
+          <div className={styles.linkContainer}>
+            <Link to="/login" className={styles.link}>Login</Link>
+            <Link to="/signup" className={styles.link}>Sign Up</Link>
+          </div>
+        </div>
       )}
     </div>
   );
